6d853c3feb6cd5e73c3c170b5cb5c5207d1bab75
Micah N Gorrell <micah@matte.(none)>
2011-11-18 19:32:16 -0700

    Use a key for Project Macaw. Will revert this when merging back to phnx.

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,13 @@
 var Config = {
-	key: '8cUPWTITE3LOKS27A1mT1A',
-	secret: 'ulz0dpskxcddc1wlraDEwPCoJYMHaNhfXJO8sMjIA0',
+	original: {
+		key: '8cUPWTITE3LOKS27A1mT1A',
+		secret: 'ulz0dpskxcddc1wlraDEwPCoJYMHaNhfXJO8sMjIA0'
+	},
+
+	key: 'wiUn8VrK6FgwQ2l5KQtLg',
+	secret: 'nQyyVimum7CRNbRT3MLNkno05YpY5KqaimcrKcLEAE',
 	bitlyUser: 'phnx',
 	bitlyKey: 'R_d120bdeaf302f28ca251ee64d7aef8fc',
-	beta: false,
-	debug: false
+	beta: true,
+	debug: true
 };
