018f32acc4cf1683964462a9bc0e02da2cf94af6
Micah N Gorrell <m@minego.net>
2012-06-21 00:41:37 -0600

    Version bump

--- app/assistants/toasters/changelog.js
+++ app/assistants/toasters/changelog.js
@@ -1,5 +1,8 @@
 var ChangelogToaster = Class.create(Toaster, {
 	items: [
+		{ version: '1.2.24' },
+		{ item: 'Display both the twitter screen name and display name in the timeline' },
+
 		{ version: '1.2.22' },
 		{ item: 'Added option to ask the user what to do when a link is clicked on. Option can be enabled in preferences, or by long pressing a URL when viewing a tweet.' },
 		{ item: 'Corrected behavior when clicking on "Tweets" when viewing a user profile' },
