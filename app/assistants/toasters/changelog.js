50e729596bd45b1d7fc47ea147a295cb2c01a1c2
Micah N Gorrell <github@minego.net>
2013-02-17 15:04:47 -0700

    Version bump, and changelog update

--- app/assistants/toasters/changelog.js
+++ app/assistants/toasters/changelog.js
@@ -1,9 +1,12 @@
 var ChangelogToaster = Class.create(Toaster, {
 	items: [
+		{ version: '1.3.7' },
+		{ item: 'Corrected sending of split tweets, which was broken in 1.3.6' },
+
 		{ version: '1.3.6' },
-		{ item: 'List/Retweet/Search timelines are now full width on TP' }, 
+		{ item: 'List/Retweet/Search timelines are now full width on TP' },
 		{ item: 'Added "via" source in search timeline' },
-		{ item: 'Changed favourite flag from "★" to "♥" to keep things consistant' }, 
+		{ item: 'Changed favourite flag from "★" to "♥" to keep things consistant' },
 		{ item: 'Option to toggle inline thumbs from showing in search timelines.  Helps to hide "unexpected" images popping up' },
 		{ item: 'Inline thumbs and emoji are now shown in search lists' },
 		{ item: '.jpg, .jpeg, .png & .gif images directly mentioned in tweets are shown as thumbnails' },
