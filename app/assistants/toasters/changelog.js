7ed28b3ce4c7b743f9b93c6533354b307e44ba38
davec <davec@lestersoftware>
2013-08-05 21:20:21 -0700

    Updated ChangeLog

--- app/assistants/toasters/changelog.js
+++ app/assistants/toasters/changelog.js
@@ -1,6 +1,7 @@
 var ChangelogToaster = Class.create(Toaster, {
 	items: [
 		{ version: '1.4.7' },
+		{ item: 'Created some additional Pre3 hires buttons/icons'},
 		{ item: 'Expanded URLs shown on profile page'},
 		{ item: 'Prelim work for "gap" support'},
 		{ item: 'Pref options added to specify number of results for Home, Mentions and Favorites timeline'},
